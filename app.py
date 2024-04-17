fa7ad2532b0f4d3360db0abd9d4cefca391a0f04
Deepak <deepakalur4@gmail.com>
2024-04-17 22:28:06 +0530

    creating a docker image

--- app.py
+++ app.py
@@ -6,11 +6,15 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("predict_page.html")
 
 @app.route("/predict",methods=["GET","POST"])
-def predict_pipeline():
-    return render_template("index.html")
+def predict():
+    if request.method=="POST":
+        return request.form["marks 1"] + request.form["marks 2"]
+    else:
+        return "No respose"
+    
 
 if __name__=="__main__":
     app.run(debug=True)
